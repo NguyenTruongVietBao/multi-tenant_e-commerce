9abbba10e30250351fed5cafe8ab3a234bbf7961
NguyenTruongVietBao <baontvde170638@fpt.edu.vn>
2026-03-10 17:03:45 +0700

    payload integration

---
+++ collections/Categories.ts
@@ -0,0 +1,6 @@
+import type { CollectionConfig } from 'payload';
+
+export const Categories: CollectionConfig = {
+  slug: 'categories',
+  fields: [{ name: 'title', type: 'text', required: true }],
+};
