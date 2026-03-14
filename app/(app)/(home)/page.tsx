f1a5af8faa3dc415dbde5a4ff237ba54857bc705
NguyenTruongVietBao <baontvde170638@fpt.edu.vn>
2026-03-14 16:09:14 +0700

    test ci cd

--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,7 +1,9 @@
 export default async function Home() {
   return (
-    <div className="text-2xl flex items-center justify-center text-blue-400">
-      Hello anh em, tôi test CI/CD
+    <div className="h-screen">
+      <div className="h-full text-2xl flex items-center justify-center text-blue-400">
+        Hello anh em, tôi test CI/CD nè nha
+      </div>
     </div>
   );
 }
